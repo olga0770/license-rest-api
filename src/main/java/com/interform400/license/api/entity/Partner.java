92f64beb44eb34662de03c779c0f79c899fec924
Olga Batrakova <olyalo0201@gmail.com>
2019-11-16 12:29:53 +0100

    Using IDENTITY as strategy in entity

--- src/main/java/com/interform400/license/api/entity/Partner.java
+++ src/main/java/com/interform400/license/api/entity/Partner.java
@@ -14,7 +14,7 @@ public class Partner {
     }
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "companyname")
--- src/main/java/com/interform400/license/api/entity/User.java
+++ src/main/java/com/interform400/license/api/entity/User.java
@@ -7,7 +7,8 @@ public class User {
 
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @GeneratedValue(strategy = GenerationType.AUTO) // issue with H2 - after inserts in data.sql, it does not know about the next available id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
